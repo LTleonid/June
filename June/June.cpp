9e881a17876b9c3ae5ce4982b370ce923838edea
LT_NoteBook <klayray10568@gmail.com>
2024-05-26 19:44:22 +0700

    Добавьте файлы проекта.

---
+++ June/June.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	char map[5][5];
+	int pos[2]{ 0,0 };
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i == pos[0] && j == pos[1]) {
+				map[i][j] = '*';
+			}
+			else {
+				map[i][j] = '#';
+			}
+			cout << map[i][j];
+		}
+		cout << endl;
+	}
+
+}
