e5a3fddc72d0b95b8c26f7d4f943280bb42aab04
LT_NoteBook <klayray10568@gmail.com>
2024-05-26 19:49:30 +0700

    asd

--- June/June.cpp
+++ June/June.cpp
@@ -1,8 +1,9 @@
 ﻿#include <iostream>
 using namespace std;
 int main() {
+	
 	char map[5][5];
-	int pos[2]{ 0,0 };
+	int pos[2]{ 2,2 };
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++) {
 			if (i == pos[0] && j == pos[1]) {
