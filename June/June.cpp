b25b391735e4bacb3ffe652535098b76e29a68c6
LT_leonid <klayray10568@gmail.com>
2024-06-20 00:03:47 +0700

    как то сломал вывод карты, рефактор игрока в Структуру

--- June/June.cpp
+++ June/June.cpp
@@ -20,35 +20,36 @@ const string COPPER = "\033[48;5;172m#\033[0m";
 const string FLOOR = "\033[48;5;28m \033[0m";
 const string ZOMBIE = "\033[38;5;22m\033[48;5;64m@\033[0m";
 
+int mapWidth = start_map_size;
+int mapHeight = start_map_size;
+
+string** map;
+
+
 struct Item {
     string name;
     int count;
 };
 
 struct Player {
-    string player_char;
+    string player_char = "LT Team";;
     int health = 100;
-    int local_x;
-    int local_y;
-    int x;
-    int y;
-
+    int local_x = 0;
+    int local_y = 0;
+    int x = start_map_size;
+    int y = start_map_size;
+    Item* backpack = new Item[0];;
+    int backpack_size = 0;
 };
 
-Item* backpack;
-int backpack_size = 0;
 
-string** map;
-int mapWidth = start_map_size;
-int mapHeight = start_map_size;
+
+
+
 
 // Данные игрока
-string player_char = "LT Team";
-int health = 100; //Добавить бар?
 enum class direction { UP = '^', DOWN = 'v', LEFT = '<', RIGHT = '>' };
 
-int local_x;
-int local_y;
 
 const int SIMULATION_MAP_X = 25;
 const int SIMULATION_MAP_y = 15;
@@ -71,15 +72,15 @@ void deleteMap(int width, int height) {
     delete[] map;
 }
 
-void expandMap(int player[2]) {
+void expandMap(Player &player) {
     int newWidth = mapWidth + 2 * EXPAND_SIZE_X;
     int newHeight = mapHeight + 2 * EXPAND_SIZE_Y;
     //Для своей устоновки координат
-    if (newWidth < player[0]) {
-        newWidth += player[0];
+    if (newWidth < player.x) {
+        newWidth += player.x;
     }
-    if (newHeight < player[1]) {
-        newHeight += player[1];
+    if (newHeight < player.y) {
+        newHeight += player.y;
     }
     string** newMap = new string * [newHeight];
     for (int y = 0; y < newHeight; ++y) {
@@ -112,42 +113,27 @@ void expandMap(int player[2]) {
     mapWidth = newWidth;
     mapHeight = newHeight;
 
-    player[0] += EXPAND_SIZE_X;
-    player[1] += EXPAND_SIZE_Y;
+    player.x += EXPAND_SIZE_X;
+    player.y += EXPAND_SIZE_Y;
 
 }
 
-void printMap(int player[2]) {
-    int startX = max(0, player[0] - vievWidth / 2);
-    int startY = max(0, player[1] - viewHeight / 2);
-    int endX = min(mapWidth, startX + vievWidth);
-    int endY = min(mapHeight, startY + viewHeight);
 
-    for (int y = startY; y < endY; ++y) {
-        for (int x = startX; x < endX; ++x) {
-            if (x == player[0] && y == player[1]) {
-                cout << "\033[38;5;20m\033[48;5;1m\033[1m" << player_char << "\033[0m";
-            }
-            else {
-                cout << map[y][x];
-            }
-        }
-        cout << endl;
-    }
-}
 
 // Методы Игрока
-void backpackMenu() {
+void backpackMenu(Player &player) {
     while (true) {
         system("cls");
         cout << "\033[38;2;158;100;0m   __________                  __    __________                  __    \n\\______   \\_____     ____  |  | __\\______   \\_____     ____  |  | __\n |    |  _/\\__  \\  _/ ___\\ |  |/ / |     ___/\\__  \\  _/ ___\\ |  |/ /\n |    |   \\ / __ \\_\\  \\___ |    <  |    |     / __ \\_\\  \\___ |    < \n |______  /(____  / \\___  >|__|_ \\ |____|    (____  / \\___  >|__|_ \\\n        \\/      \\/      \\/      \\/                \\/      \\/      \\/\033[0m";
         cout << "\n\nBackpack Contents:\n";
-        for (int i = 0; i < backpack_size; ++i) {
+        for (int i = 0; i < player.backpack_size; ++i) {
             
         }
        
         switch (_getch())
         {
+        case 'w':
+            break;
         default:
             break;
         }
@@ -155,80 +141,80 @@ void backpackMenu() {
     }
 }
 
-void addItem(string item_name) {
-    for (int i = 0; i < backpack_size; ++i) {
-        if (backpack[i].name == item_name) {
-            backpack[i].count++;
+void addItem(Player &player, string item_name) {
+    for (int i = 0; i < player.backpack_size; ++i) {
+        if (player.backpack[i].name == item_name) {
+            player.backpack[i].count++;
             return;
         }
     }
-    Item* new_backpack = new Item[backpack_size + 1];
-    for (int i = 0; i < backpack_size; ++i) {
-        new_backpack[i] = backpack[i];
+    Item* new_backpack = new Item[player.backpack_size + 1];
+    for (int i = 0; i < player.backpack_size; ++i) {
+        new_backpack[i] = player.backpack[i];
     }
-    new_backpack[backpack_size].name = item_name;
-    new_backpack[backpack_size].count = 1;
-    delete[] backpack;
-    backpack = new_backpack;
-    backpack_size++;
+    new_backpack[player.backpack_size].name = item_name;
+    new_backpack[player.backpack_size].count = 1;
+    delete[] player.backpack;
+    player.backpack = new_backpack;
+    player.backpack_size++;
 }
 
-void destroy(int player[2]) {
-    int x = player[0];
-    int y = player[1];
+void destroy(Player &player) {
+    int x = player.x;
+    int y = player.y;
 
-    if (player_char == "^" && y > 0) y--;
-    else if (player_char == "v" && y < mapHeight - 1) y++;
-    else if (player_char == ">" && x < mapWidth - 1) x++;
-    else if (player_char == "<" && x > 0) x--;
+    if (player.player_char == "^" && y > 0) y--;
+    else if (player.player_char == "v" && y < mapHeight - 1) y++;
+    else if (player.player_char == ">" && x < mapWidth - 1) x++;
+    else if (player.player_char == "<" && x > 0) x--;
 
     if (map[y][x] != FLOOR) {
-        addItem(map[y][x]);
+        addItem(player, map[y][x]);
         map[y][x] = FLOOR;
     }
 }
 
-void movePlayer(int player[2], int move) {
+void movePlayer(Player &player, int move) {
     switch (move) {
     case 'W':
     case 'w':
-        if (player[1] > 0) player[1]--;
-        player_char = (char)direction::UP;
-        local_y--;
+        if (player.y > 0) player.y--;
+        player.player_char = (char)direction::UP;
+        player.local_y--;
         break;
     case 'S':
     case 's':
-        if (player[1] < mapHeight - 1) player[1]++;
-        player_char = (char)direction::DOWN;
-        local_y++;
+        if (player.y < mapHeight - 1) player.y++;
+        player.player_char = (char)direction::DOWN;
+        player.local_y++;
         break;
     case 'A':
     case 'a':
-        if (player[0] > 0) player[0]--;
-        player_char = (char)direction::LEFT;
-        local_x--;
+        if (player.x > 0) player.x--;
+        player.player_char = (char)direction::LEFT;
+        player.local_x--;
         break;
     case 'D':
     case 'd':
-        if (player[0] < mapWidth - 1) player[0]++;
-        player_char = (char)direction::RIGHT;
-        local_x++;
+        if (player.x < mapWidth - 1) player.x++;
+        player.player_char = (char)direction::RIGHT;
+        player.local_x++;
         break;
     case 'B':
     case 'b':
-        backpackMenu();
+        backpackMenu(player);
         break;
     case 72: // Вверх стрелка
-        player_char = (char)direction::UP;
+        player.player_char = (char)direction::UP;
         break;
     case 80: // Вниз стрелка
-        player_char = (char)direction::DOWN;
+        player.player_char = (char)direction::DOWN;
         break;
     case 75: // Влево стрелка
-        player_char = (char)direction::LEFT;
+        player.player_char = (char)direction::LEFT;
         break;
     case 77: // Вправо стрелка
-        player_char = (char)direction::RIGHT;
+        player.player_char = (char)direction::RIGHT;
         break;
     case 32:
         destroy(player);
@@ -237,8 +223,25 @@ void movePlayer(int player[2], int move) {
         break;
     }
 
-    if (player[0] <= EXPAND_SIZE_X || player[1] <= EXPAND_SIZE_Y || player[0] >= mapWidth - EXPAND_SIZE_X || player[1] >= mapHeight - EXPAND_SIZE_Y) {
+    if (player.x <= EXPAND_SIZE_X || player.y <= EXPAND_SIZE_Y || player.x >= mapWidth - EXPAND_SIZE_X || player.y >= mapHeight - EXPAND_SIZE_Y) {
         expandMap(player);
+    }
+}
+
+void printMap(Player& player) {
+    for (int y = player.y - EXPAND_SIZE_Y; y < player.y + EXPAND_SIZE_Y; ++y) {
+        for (int x = player.x - EXPAND_SIZE_X; x < player.x + EXPAND_SIZE_X; ++x) {
+            if (x == player.x && y == player.y) {
+                cout << player.player_char;
+            }
+            else if (x >= 0 && x < mapWidth && y >= 0 && y < mapHeight) {
+                cout << map[y][x];
+            }
+            else {
+                cout << FLOOR;
+            }
+        }
+        cout << endl;
     }
 }
 
@@ -251,10 +254,9 @@ void start() {
 
     srand(time(NULL));
     generateMap(start_map_size, start_map_size);
-    int player[2] = { start_map_size / 2, start_map_size / 2 };
-    local_x = player[0]; local_y = player[1];
+    Player player;
+   
 
-    backpack = new Item[0];
 
     while (true) {
         if (tickCounter == TICK) tickCounter = 0;
@@ -265,8 +267,8 @@ void start() {
             movePlayer(player, move);
         }
 
-        cout << endl << "lX: " << local_x << " | lY: " << local_y;
-        cout << endl << "gX: " << player[0] << " | gY: " << player[1];
+        cout << endl << "lX: " << player.local_x << " | lY: " << player.local_y;
+        cout << endl << "gX: " << player.x << " | gY: " << player.y;
         cout << endl << "XM: " << mapWidth << " | YM: " << mapHeight;
         cout << endl << "Memory_Map: " << ((mapHeight * mapWidth) / 1024 / 1024);
         cout << endl << "Tick: " << tickCounter;
@@ -275,7 +277,7 @@ void start() {
     }
 
     deleteMap(mapWidth, mapHeight);
-    delete[] backpack;
+    delete[] player.backpack;
 }
 int main() {
     start();
