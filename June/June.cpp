1f1388d7607512d1716f018e07c979bf7e85b1ae
LT_NoteBook <klayray10568@gmail.com>
2024-06-24 19:56:59 +0700

    #

--- June/June.cpp
+++ June/June.cpp
@@ -7,19 +7,23 @@ using namespace std;
 const int TICK = 60;
 int tickCounter = 0;
 // Данные карты
-int vievWidth = 104;
+int vievWidth = 20;
 int viewHeight = 10;
 
-const int start_map_size = 1;
+const int start_map_size = 100;
 
 const int EXPAND_SIZE_X = vievWidth / 2;
 const int EXPAND_SIZE_Y = viewHeight * 2;
 
 const string TREE = "\033[48;5;22m^\033[0m";
 const string COPPER = "\033[48;5;172m#\033[0m";
+const string ROCK = "\033[38;5;7m\033[48;5;240m#\033[0m";
 const string FLOOR = "\033[48;5;28m \033[0m";
+
 const string ZOMBIE = "\033[38;5;22m\033[48;5;64m@\033[0m";
 
+const int MOB_CAP = 70;
+int mobCount = 0;
 int mapWidth = start_map_size;
 int mapHeight = start_map_size;
 
@@ -30,10 +34,10 @@ struct Item {
     string name;
     int count;
 };
-
+// Данные сущностей
 struct Player {
     string player_char = "LT Team";;
-    int health = 100;
+    int health = 90;
     int local_x = 0;
     int local_y = 0;
     int x = start_map_size;
@@ -42,26 +46,28 @@ struct Player {
     int backpack_size = 0;
 };
 
+struct Enemy {
+    string enemy_char;
+    int health;
+    int x;
+    int y;
+    int damage;
+    int detectRadius;
+};
 
 
-
-
-
-// Данные игрока
+Enemy mob_list[MOB_CAP];
 enum class direction { UP = '^', DOWN = 'v', LEFT = '<', RIGHT = '>' };
 
-
 const int SIMULATION_MAP_X = 25;
-const int SIMULATION_MAP_y = 15;
+const int SIMULATION_MAP_Y = 15;
+
 
 // Методы Карты
 void generateMap(int width, int height) {
     map = new string * [height];
     for (int y = 0; y < height; ++y) {
         map[y] = new string[width];
-        for (int x = 0; x < width; ++x) {
-            map[y][x] = FLOOR;
-        }
     }
 }
 
@@ -94,7 +100,17 @@ void expandMap(Player &player) {
             }
             else {
                 int randNum = rand() % 100;
-                if (randNum < 10) {
+                if (randNum < 1) {
+                    if (mobCount < MOB_CAP) {
+                        newMap[y][x] = ZOMBIE;
+                        mob_list[mobCount] = { ZOMBIE, 100, x, y, 25, 5};
+                        mobCount++;
+                    }
+                }
+                else if (randNum < 9) {
+                    newMap[y][x] = ROCK;
+                }
+                else if (randNum < 10) {
                     newMap[y][x] = TREE;
                 }
                 else if (randNum < 11) {
@@ -103,6 +119,7 @@ void expandMap(Player &player) {
                 else {
                     newMap[y][x] = FLOOR;
                 }
+
             }
         }
     }
@@ -118,16 +135,48 @@ void expandMap(Player &player) {
 
 }
 
+bool inRadius(Enemy& mob, Player& player) {
+    for (int y = mob.y - mob.detectRadius; y < mob.y + mob.detectRadius; ++y) {
+        for (int x = mob.x - mob.detectRadius; x < mob.x + mob.detectRadius; ++x) {
+            if (y == player.y && x == player.x) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
+void moveEnemy(Player& player) {
+    Enemy mob;
+    for (int i = 0; i < mobCount; i++) {
+        mob = mob_list[i];
+        if (inRadius(mob, player)) {
+            continue;
+        }
+        else {
+            switch ((rand() % 4)) {
+            case 0:
+                mob.x--;
+            case 1:
+                mob.x++;
+            case 2:
+                mob.y--;
+            case 3:
+                mob.y++;
+            }
+            
+        }
+    }
+}
 
 // Методы Игрока
 void backpackMenu(Player &player) {
     while (true) {
         system("cls");
-        cout << "\t\033[38;2;158;100;0m   __________                  __    __________                  __    \n\\______   \\_____     ____  |  | __\\______   \\_____     ____  |  | __\n |    |  _/\\__  \\  _/ ___\\ |  |/ / |     ___/\\__  \\  _/ ___\\ |  |/ /\n |    |   \\ / __ \\_\\  \\___ |    <  |    |     / __ \\_\\  \\___ |    < \n |______  /(____  / \\___  >|__|_ \\ |____|    (____  / \\___  >|__|_ \\\n        \\/      \\/      \\/      \\/                \\/      \\/      \\/\033[0m";
-        cout << "\n\nBackpack Contents:\n";
+        cout << "\033[38;2;158;100;0m   __________                  __    __________                  __    \n\\______   \\_____     ____  |  | __\\______   \\_____     ____  |  | __\n |    |  _/\\__  \\  _/ ___\\ |  |/ / |     ___/\\__  \\  _/ ___\\ |  |/ /\n |    |   \\ / __ \\_\\  \\___ |    <  |    |     / __ \\_\\  \\___ |    < \n |______  /(____  / \\___  >|__|_ \\ |____|    (____  / \\___  >|__|_ \\\n        \\/      \\/      \\/      \\/                \\/      \\/      \\/\033[0m";
         for (int i = 0; i < player.backpack_size; ++i) {
-            
+            if (i % 3 == 0) cout << "\n";
+            cout << player.backpack[i].name << " x" << player.backpack[i].count << " | "  ;
         }
        
         switch (_getch())
@@ -229,10 +278,11 @@ void movePlayer(Player &player, int move) {
 }
 
 void printMap(Player& player) {
-    if (tickCounter % 2 == 0) {
+
         system("cls");
         for (int y = player.y - viewHeight; y < player.y + viewHeight; ++y) {
             for (int x = player.x - vievWidth; x < player.x + vievWidth; ++x) {
+                
                 if (x == player.x && y == player.y) {
                     cout << player.player_char;
                 }
@@ -245,11 +295,32 @@ void printMap(Player& player) {
             }
             cout << endl;
         }
+        /*for (int y = player.y - SIMULATION_MAP_Y; y < player.y + SIMULATION_MAP_Y; ++y) {
+            for (int x = player.x - SIMULATION_MAP_X; x < player.x + SIMULATION_MAP_X; ++x) {
+                if (map[y][x] == ZOMBIE) {
+                    moveEnemy(player);
+                }
+            }
+        }*/
+}
+
+void gui(Player& player) {
+    cout << "\033[48;2;0;255;0m\033[38;2;255;0;0m";
+    cout << player.health;
+    for (int i = 0; i < player.health; i++) {
+        
+        if (i % 5 == 0) {
+            cout << " ";
+        }
+       
     }
+    cout << "\033[0m";
+    cout << endl;
+    
 }
 
 void start() {
-    cout << "\033[37,5,4m";
+
     int move = ' ';
 
     SetConsoleOutputCP(CP_UTF8);
@@ -258,25 +329,24 @@ void start() {
     srand(time(NULL));
     generateMap(start_map_size, start_map_size);
     Player player;
-   
+
 
 
     while (true) {
-        if (tickCounter == TICK) tickCounter = 0;
         
         printMap(player);
         if (_kbhit()) {
             move = _getch();
             movePlayer(player, move);
         }
-
+        gui(player);
         cout << endl << "lX: " << player.local_x << " | lY: " << player.local_y;
         cout << endl << "gX: " << player.x << " | gY: " << player.y;
         cout << endl << "XM: " << mapWidth << " | YM: " << mapHeight;
         cout << endl << "Memory_Map: " << ((mapHeight * mapWidth) / 1024 / 1024);
         cout << endl << "Tick: " << tickCounter << endl;
-        Sleep(1000 / TICK);
-        tickCounter++;
+        Sleep(35);
+
     }
 
     deleteMap(mapWidth, mapHeight);
